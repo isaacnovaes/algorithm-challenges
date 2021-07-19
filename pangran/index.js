207c52fbd9826e8fb4f83fb22289aca4cc5f8d15
Isaac Novaes <isaac.novaes@hotmail.com>
2021-07-19 21:40:42 +0200

    Update index.js

--- pangran/index.js
+++ pangran/index.js
@@ -1,4 +1,5 @@
-// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+// A pangram is a sentence that contains every single letter of the alphabet at least once.
+// For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
 
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
@@ -10,6 +11,8 @@ function isPangram(string) {
 	}
 	return true;
 }
-document.getElementById("demo").innerHTML = isPangram(
+
+
+isPangram(
 	"The quick brown fox jumps over the lazy dog"
 );
