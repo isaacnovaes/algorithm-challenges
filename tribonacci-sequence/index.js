de6d88678eec18cd7be17250bf771b6508cf395f
Isaac Novaes <isaac.novaes@hotmail.com>
2021-08-02 23:25:48 +0200

    Challenge is finished

--- tribonacci-sequence/index.js
+++ tribonacci-sequence/index.js
@@ -1,3 +1,20 @@
 function tribonacci(signature, n) {
-	//your code here
+	const arr = [...signature];
+
+	switch (n) {
+		case 0:
+			return [];
+		case 1:
+			return [arr[0]];
+		case 2:
+			return [arr[0], arr[1]];
+		case 3:
+			return arr;
+	}
+
+	for (let index = 3; index < n; index++) {
+		arr[index] = arr[index - 1] + arr[index - 2] + arr[index - 3];
+	}
+
+	return arr;
 }
